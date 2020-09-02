2366c31c07b21671e1aa3d9bb93e2a7b81316125
Crauzer <filip.quitko@gmail.com>
2020-09-02 13:00:03 +0200

    initial

---
+++ lol2gltf/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lol2gltf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

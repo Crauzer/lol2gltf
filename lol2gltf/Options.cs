b5ffea7e4dccbc27cdd6f0848df07e4d140ca2ff
Crauzer <filip.quitko@gmail.com>
2020-09-06 14:55:06 +0200

    simplify arguments

--- lol2gltf/Options.cs
+++ lol2gltf/Options.cs
@@ -9,19 +9,19 @@ namespace lol2gltf
 {
     public interface IBaseSimpleSkinToGltfOptions
     {
-        [Option("simple-skin", Default = null, Required = true, HelpText = "Path to the SKN (Simple Skin) file")]
+        [Option('m', "skn", Required = true, HelpText = "Path to the SKN (Simple Skin) file")]
         public string SimpleSkinPath { get; set; }
 
-        [Option("material-names", Default = null, Required = false,
+        [Option("material-names", Required = false,
             HelpText =
             "Names of the materials you want to assign a texture to using the texture-paths argument\n" +
             "Textures will be applies to materials in sequential order specified by this argument")]
         public IEnumerable<string> TextureMaterialNames { get; set; }
 
-        [Option("texture-paths", Default = null, Required = false, HelpText = "Paths to the textures used by the material-names argument")]
+        [Option("texture-paths", Required = false, HelpText = "Paths to the textures used by the material-names argument")]
         public IEnumerable<string> MaterialTexturePaths { get; set; }
 
-        [Option('o', "output-path", Default = null, Required = true,
+        [Option('o', "output-path", Required = true,
             HelpText = 
             "Where the glTF file should be saved.\n" +
             "Use .gltf extension to save a JSON .glTF file with a resoruce file (.bin)\n" +
@@ -41,7 +41,7 @@ namespace lol2gltf
         public string OutputPath { get; set; }
     }
 
-    [Verb("convert-simple-skin", HelpText = "Converts an SKN (Simple Skin) file to a glTF with optional textures")]
+    [Verb("skn2gltf", HelpText = "Converts an SKN (Simple Skin) file to a glTF with optional textures")]
     public class SimpleSkinToGltfOptions : IBaseSimpleSkinToGltfOptions
     {
         public string SimpleSkinPath { get; set; }
@@ -50,7 +50,7 @@ namespace lol2gltf
         public string OutputPath { get; set; }
     }
 
-    [Verb("convert-skinned-model", HelpText = "Converts a SKN (Simple Skin) file with a SKL (Skeleton) file to a glTF with optional textures and animations")]
+    [Verb("skn2gltf-rigged", HelpText = "Converts an SKN (Simple Skin) file with a SKL (Skeleton) file to a glTF with optional textures and animations")]
     public class SkinnedModelToGltfOptions : IBaseSimpleSkinToGltfOptions
     {
         public string SimpleSkinPath { get; set; }
@@ -58,30 +58,30 @@ namespace lol2gltf
         public IEnumerable<string> MaterialTexturePaths { get; set; }
         public string OutputPath { get; set; }
 
-        [Option("skeleton", Default = null, Required = true, HelpText = "Path to the SKL (Skeleton) file")]
+        [Option('s', "skeleton", Required = true, HelpText = "Path to the SKL (Skeleton) file")]
         public string SkeletonPath { get; set; }
 
-        [Option("animations-folder", Group = "animations", Default = null, HelpText = "Path to the folder which contains the ANM (Animation) files used for conversion")]
+        [Option("animations-folder", Group = "animations", HelpText = "Path to the folder which contains the ANM (Animation) files used for conversion")]
         public string AnimationsFolder { get; set; }
 
-        [Option("animations", Group = "animations", Default = null, HelpText = "Paths to the ANM (Animation) files used for conversion")]
+        [Option("animations", Group = "animations", HelpText = "Paths to the ANM (Animation) files used for conversion")]
         public IEnumerable<string> AnimationPaths { get; set; }
     }
 
-    [Verb("dump-simple-skin", HelpText = "Displays all data about the provided SKN (Simple Skin)")]
+    [Verb("dump-skn", HelpText = "Displays all data about the provided SKN (Simple Skin)")]
     public class DumpSimpleSkinInfoOptions
     {
-        [Option('s', "simple-skin", Default = null, Required = true, HelpText = "Path to the SKN (Simple Skin) file")]
+        [Option('s', "skn", Required = true, HelpText = "Path to the SKN (Simple Skin) file")]
         public string SimpleSkinPath { get; set; }
     }
 
-    [Verb("create-skn-from-legacy", HelpText = "Creates a Simple Skin (SKN) from legacy SCO + WGT file combination")]
+    [Verb("legacy2skn", HelpText = "Creates a Simple Skin (SKN) from legacy SCO + WGT file combination")]
     public class CreateSimpleSkinFromLegacyOptions
     {
-        [Option("sco", Required = true, HelpText = "Path to the SCO file")]
+        [Option('s', "sco", Required = true, HelpText = "Path to the SCO file")]
         public string StaticObjectPath { get; set; }
 
-        [Option("wgt", Required = true, HelpText = "Path to the WGT file")]
+        [Option('w', "wgt", Required = true, HelpText = "Path to the WGT file")]
         public string WeightFilePath { get; set; }
 
         [Option('o', "output-path", Required = true, HelpText = "Path where to save the created SKN")]

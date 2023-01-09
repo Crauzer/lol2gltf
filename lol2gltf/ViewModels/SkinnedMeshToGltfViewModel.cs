27e6e7b93e17fe1ca7fe500acdba6ee3861ba7c6
Crauzer <filip.quitko@gmail.com>
2023-01-09 01:31:18 +0100

    export to gltf

--- lol2gltf/ViewModels/SkinnedMeshToGltfViewModel.cs
+++ lol2gltf/ViewModels/SkinnedMeshToGltfViewModel.cs
@@ -15,41 +15,143 @@ using lol2gltf.Views;
 using Splat;
 using Avalonia;
 using System.Windows;
+using System.Reactive.Linq;
+using LeagueToolkit.IO.SkeletonFile;
+using System.Collections.ObjectModel;
+using Avalonia.Collections;
+using DynamicData.Binding;
+using LeagueToolkit.IO.SimpleSkinFile;
+using CommunityToolkit.Diagnostics;
+using SharpGLTF.Schema2;
+
+using LeagueAnimation = LeagueToolkit.IO.AnimationFile.Animation;
+using DynamicData;
 
 namespace lol2gltf.ViewModels
 {
-    public sealed class SkinnedMeshToGltfViewModel : PageViewModel
+    public sealed class SkinnedMeshToGltfViewModel : PageViewModel, IActivatableViewModel
     {
+        public ViewModelActivator Activator { get; } = new();
+
         [Reactive]
         public string SimpleSkinPath { get; set; }
 
         [Reactive]
         public string SkeletonPath { get; set; }
 
-        public ReactiveCommand<Unit, (string path, SkinnedMesh skinnedMesh)> OnSelectSimpleSkinCommand { get; }
+        public SkinnedMesh SkinnedMesh => this._skinnedMesh.Value;
+        public Skeleton Skeleton => this._skeleton.Value;
 
-        private readonly IDialogService _dialogService;
+        [Reactive]
+        public ObservableCollection<SkinnedMeshPrimitiveViewModel> SkinnedMeshPrimitives { get; set; } = new();
 
-        public SkinnedMeshToGltfViewModel(IDialogService dialogService = null)
-            : this(
-                dialogService ?? Locator.Current.GetService<IDialogService>(),
-                "Skinned Mesh to glTf",
-                "",
-                Symbol.OpenFile
-            ) { }
+        private ObservableAsPropertyHelper<SkinnedMesh> _skinnedMesh;
+        private ObservableAsPropertyHelper<Skeleton> _skeleton;
 
-        public SkinnedMeshToGltfViewModel(IDialogService dialogService, string name, string tooltip, Symbol icon)
-            : base(name, tooltip, icon)
+        public ReactiveCommand<Unit, Unit> OnSelectSimpleSkinCommand { get; }
+        public ReactiveCommand<Unit, Unit> OnSelectSkeletonCommand { get; }
+        public ReactiveCommand<string, Unit> OnExportGltfCommand { get; }
+
+        public Interaction<Unit, string> ShowLoadSimpleSkinDialog { get; }
+        public Interaction<Unit, string> ShowLoadSkeletonDialog { get; }
+        public Interaction<string, string> ShowExportGltfDialog { get; }
+
+        public SkinnedMeshToGltfViewModel() : this("Skinned Mesh to glTf", "", Symbol.OpenFile) { }
+
+        public SkinnedMeshToGltfViewModel(string name, string tooltip, Symbol icon) : base(name, tooltip, icon)
         {
-            this._dialogService = dialogService;
-            this.OnSelectSimpleSkinCommand = ReactiveCommand.CreateFromTask(LoadSimpleSkin);
+            this.OnSelectSimpleSkinCommand = ReactiveCommand.CreateFromTask(LoadSimpleSkinAsync);
+            this.OnSelectSkeletonCommand = ReactiveCommand.CreateFromTask(LoadSkeletonAsync);
+            this.OnExportGltfCommand = ReactiveCommand.CreateFromTask<string>(ExportGltfAsync);
+
+            this.ShowLoadSimpleSkinDialog = new();
+            this.ShowLoadSkeletonDialog = new();
+            this.ShowExportGltfDialog = new();
+
+            this._skinnedMesh = this.WhenAnyValue(x => x.SimpleSkinPath)
+                .Select(
+                    x =>
+                        x switch
+                        {
+                            null => null,
+                            string simpleSkinPath => SkinnedMesh.ReadFromSimpleSkin(simpleSkinPath)
+                        }
+                )
+                .ToProperty(this, nameof(this.SkinnedMesh));
+
+            this._skeleton = this.WhenAnyValue(x => x.SkeletonPath)
+                .Select(
+                    x =>
+                        x switch
+                        {
+                            null => null,
+                            string skeletonPath => new Skeleton(skeletonPath)
+                        }
+                )
+                .ToProperty(this, nameof(this.Skeleton));
+
+            this.WhenValueChanged(x => x.SkinnedMesh)
+                .Subscribe(skinnedMesh =>
+                {
+                    this.SkinnedMeshPrimitives.Clear();
+
+                    if (skinnedMesh is not null)
+                    {
+                        this.SkinnedMeshPrimitives.AddRange(
+                            skinnedMesh.Ranges.Select(
+                                range =>
+                                    new SkinnedMeshPrimitiveViewModel()
+                                    {
+                                        Material = range.Material,
+                                        VertexCount = range.VertexCount,
+                                        FaceCount = range.IndexCount / 3
+                                    }
+                            )
+                        );
+                    }
+                });
         }
 
-        private async Task<(string path, SkinnedMesh skinnedMesh)> LoadSimpleSkin()
+        private async Task LoadSimpleSkinAsync()
         {
-            var x = this._dialogService.ShowLoadSkinnedMeshViewAsync(App.MainWindow);
+            string path = await this.ShowLoadSimpleSkinDialog.Handle(new());
 
-            return await x;
+            this.SimpleSkinPath = path;
         }
+
+        private async Task LoadSkeletonAsync()
+        {
+            string path = await this.ShowLoadSkeletonDialog.Handle(new());
+
+            this.SkeletonPath = path;
+        }
+
+        private async Task ExportGltfAsync(string extension)
+        {
+            if (extension is not ".glb" or ".gltf")
+                throw new ArgumentException($"Invalid extension: {extension}", nameof(extension));
+
+            string path = await this.ShowExportGltfDialog.Handle(extension);
+            if (string.IsNullOrEmpty(path))
+                return; // User canceled
+
+            Dictionary<string, ReadOnlyMemory<byte>> materialTextures = new();
+            List<(string name, LeagueAnimation animation)> animations = new();
+
+            ModelRoot gltfAsset = this.Skeleton switch
+            {
+                null => this.SkinnedMesh.ToGltf(materialTextures),
+                Skeleton skeleton => this.SkinnedMesh.ToGltf(skeleton, materialTextures, animations)
+            };
+
+            gltfAsset.Save(path);
+        }
+    }
+
+    public class SkinnedMeshPrimitiveViewModel
+    {
+        public string Material { get; set; }
+        public int VertexCount { get; set; }
+        public int FaceCount { get; set; }
     }
 }
--- lol2gltf/Views/SkinnedMeshToGltfView.axaml.cs
+++ lol2gltf/Views/SkinnedMeshToGltfView.axaml.cs
@@ -1,12 +1,101 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.ReactiveUI;
+using LeagueToolkit.Core.Mesh;
+using LeagueToolkit.IO.SkeletonFile;
+using lol2gltf.ViewModels;
+using ReactiveUI;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
 
 namespace lol2gltf.Views
 {
-    public partial class SkinnedMeshToGltfView : UserControl
+    public partial class SkinnedMeshToGltfView : ReactiveUserControl<SkinnedMeshToGltfViewModel>
     {
         public SkinnedMeshToGltfView()
         {
             InitializeComponent();
+            this.WhenActivated(d =>
+            {
+                d.Invoke(this.ViewModel.ShowLoadSimpleSkinDialog.RegisterHandler(DoShowLoadSimpleSkinDialogAsync));
+                d.Invoke(this.ViewModel.ShowLoadSkeletonDialog.RegisterHandler(DoShowLoadSkeletonDialogAsync));
+                d.Invoke(this.ViewModel.ShowExportGltfDialog.RegisterHandler(DoShowExportGltfDialogAsync));
+            });
+        }
+
+        private async Task DoShowLoadSimpleSkinDialogAsync(InteractionContext<Unit, string> interaction)
+        {
+            OpenFileDialog dialog =
+                new()
+                {
+                    AllowMultiple = false,
+                    Title = "aaa",
+                    Filters = new()
+                    {
+                        new()
+                        {
+                            Name = "Simple Skin files (.skn)",
+                            Extensions = new() { "skn" }
+                        }
+                    }
+                };
+
+            string[] files = await dialog.ShowAsync(
+                ((ClassicDesktopStyleApplicationLifetime)Application.Current.ApplicationLifetime).MainWindow
+            );
+
+            interaction.SetOutput(files?.FirstOrDefault());
+        }
+
+        private async Task DoShowLoadSkeletonDialogAsync(InteractionContext<Unit, string> interaction)
+        {
+            OpenFileDialog dialog =
+                new()
+                {
+                    AllowMultiple = false,
+                    Title = "aaa",
+                    Filters = new()
+                    {
+                        new()
+                        {
+                            Name = "Skeleton files (.skl)",
+                            Extensions = new() { "skl" }
+                        }
+                    }
+                };
+
+            string[] files = await dialog.ShowAsync(
+                ((ClassicDesktopStyleApplicationLifetime)Application.Current.ApplicationLifetime).MainWindow
+            );
+
+            interaction.SetOutput(files?.FirstOrDefault());
+        }
+
+        private async Task DoShowExportGltfDialogAsync(InteractionContext<string, string> interaction)
+        {
+            SaveFileDialog dialog =
+                new()
+                {
+                    DefaultExtension = interaction.Input,
+                    Filters = new()
+                    {
+                        new()
+                        {
+                            Name = $"glTF files ({interaction.Input})",
+                            Extensions = new() { interaction.Input }
+                        }
+                    }
+                };
+
+            string file = await dialog.ShowAsync(
+                ((ClassicDesktopStyleApplicationLifetime)Application.Current.ApplicationLifetime).MainWindow
+            );
+
+            interaction.SetOutput(file);
         }
     }
 }

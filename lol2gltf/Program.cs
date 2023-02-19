c920bd69eca0d28f5c781b0878ad8c72edcc37c6
Crauzer <18646077+Crauzer@users.noreply.github.com>
2023-02-19 15:07:00 +0100

    use photino

---
+++ lol2gltf/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MudBlazor;
+using MudBlazor.Services;
+using Photino.Blazor;
+
+namespace lol2gltf;
+
+public class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        PhotinoBlazorAppBuilder builder = PhotinoBlazorAppBuilder.CreateDefault(args);
+
+        builder.Services.AddLogging();
+
+        // register root component and selector
+        builder.RootComponents.Add<App>("app");
+
+        builder.Services.AddMudServices(config =>
+        {
+            config.SnackbarConfiguration.PreventDuplicates = true;
+            config.SnackbarConfiguration.ShowCloseIcon = true;
+            config.SnackbarConfiguration.VisibleStateDuration = 5000;
+            config.SnackbarConfiguration.ShowTransitionDuration = 250;
+            config.SnackbarConfiguration.HideTransitionDuration = 250;
+            config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+            config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomCenter;
+        });
+
+        PhotinoBlazorApp app = builder.Build();
+
+        // customize window
+        app.MainWindow.SetIconFile("favicon.ico").SetTitle("lol2gltf");
+
+        AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+        {
+            app.MainWindow.OpenAlertWindow("Fatal exception", error.ExceptionObject.ToString());
+        };
+
+        app.Run();
+    }
+}

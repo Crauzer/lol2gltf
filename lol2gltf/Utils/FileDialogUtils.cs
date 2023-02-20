f015f12cd73ab87a63704239f9c52f39b4e673a0
Crauzer <18646077+Crauzer@users.noreply.github.com>
2023-02-20 14:57:59 +0100

    cleanup

--- lol2gltf/Utils/FileDialogUtils.cs
+++ lol2gltf/Utils/FileDialogUtils.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace lol2gltf.Utils;
 
